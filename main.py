ae7b5b4ef85d47e939d424db48be77d5f45674ad
Tool-3 <tool.3074@gmail.com>
2024-06-23 00:19:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def extract_text_api(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=1000)
